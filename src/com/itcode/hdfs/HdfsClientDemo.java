6871791d991889b40406e84c31aa10f536d9a838
sunalong <sunalong@ztgame.com>
2017-09-18 12:15:20 +0800

    javaApi 上传文件到hdfs并下载

---
+++ src/com/itcode/hdfs/HdfsClientDemo.java
@@ -0,0 +1,36 @@
+package com.itcode.hdfs;
+
+import org.apache.hadoop.conf.Configuration;
+import org.apache.hadoop.fs.FileSystem;
+import org.apache.hadoop.fs.Path;
+import org.junit.Before;
+import org.junit.Test;
+
+import java.io.IOException;
+import java.net.URI;
+import java.net.URISyntaxException;
+
+/**
+ * Created by along on 17/6/3.
+ * 客户端操作hdfs,
+ */
+public class HdfsClientDemo {
+        Configuration configuration;
+        FileSystem fileSystem;
+    @Before
+    public void init() throws Exception {
+        configuration = new Configuration();
+        configuration.set("fs.defaultFS","hdfs://mini1:9000");
+        fileSystem = FileSystem.get(new URI("hdfs://mini1:9000"), configuration, "hadoop");
+    }
+    @Test
+    public void testUpload() throws IOException {
+        fileSystem.copyFromLocalFile(new Path("/Users/along/ATest/copyToHdfs"),new Path("/JavaAPIWithHdfs.txt"));
+        fileSystem.close();
+    }
+    @Test
+    public void testDownload() throws IOException {
+        fileSystem.copyToLocalFile(new Path("/jdk-8u131-linux-x64.tar.gz"),new Path("/Users/along/ATest/downJdk.tar.gz"));
+        fileSystem.close();
+    }
+}
