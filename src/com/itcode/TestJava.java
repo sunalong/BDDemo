f06c7cb3706a606a867d013319c1100f2ed19307
sunalong <sunalong@ztgame.com>
2017-09-18 11:36:25 +0800

    更新 .gitignore规则

---
+++ src/com/itcode/TestJava.java
@@ -0,0 +1,12 @@
+package com.itcode;
+
+/**
+ * Created by along on 17/9/18 11:21.
+ * Email:466210864@qq.com
+ */
+public class TestJava {
+
+    public static void main(String[] args) {
+        System.out.print(true + "<--->" + false);
+    }
+}
