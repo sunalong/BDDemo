0fd464e34c0bc2ad674f57a912cec17c26eccf91
sunalong <sunalong@ztgame.com>
2017-09-28 12:15:18 +0800

    更改为maven项目

--- src/com/itcode/TestJava.java
+++ src/com/itcode/TestJava.java
@@ -1,5 +1,7 @@
 package com.itcode;
 
+import org.junit.Test;
+
 /**
  * Created by along on 17/9/18 11:21.
  * Email:466210864@qq.com
@@ -8,5 +10,9 @@ public class TestJava {
 
     public static void main(String[] args) {
         System.out.print(true + "<--->" + false);
+    }
+    @Test
+    public void testJunit(){
+        System.out.print("this is test Junit");
     }
 }
--- src/com/itcode/hbase/PDetailBean.java
+++
@@ -1,9 +0,0 @@
-package com.itcode.hbase;
-
-/**
- * Created by along on 17/9/21 10:52.
- * Email:466210864@qq.com
- */
-public class PDetailBean {
-
-}
