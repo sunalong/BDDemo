b6608d1157b9c11218e67afadb98c7834c5c9db7
sunalong <sunalong@ztgame.com>
2017-09-29 11:10:27 +0800

    为爬取的网站生成索引

--- src/com/itcode/lucene/CreateIndex.java
+++ src/com/itcode/lucene/CreateIndex.java
@@ -1,16 +1,19 @@
 package com.itcode.lucene;
 
 import org.apache.commons.io.FileUtils;
+import org.apache.commons.io.filefilter.TrueFileFilter;
 import org.apache.lucene.analysis.standard.StandardAnalyzer;
 import org.apache.lucene.document.*;
 import org.apache.lucene.index.IndexWriter;
 import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.index.IndexableField;
 import org.apache.lucene.store.FSDirectory;
 import org.apache.lucene.util.Version;
 import org.junit.Test;
 
 import java.io.File;
 import java.io.IOException;
+import java.util.Collection;
 
 /**
  * Created by along on 17/9/28 14:42.
@@ -19,6 +22,30 @@ import java.io.IOException;
 public class CreateIndex {
     public static final String indexDir = "/Users/along/ATest/index";
     public static final String dataDir = "/Users/along/ATest/data";
+    public static final String siteDir = "/Users/along/ATest/gnn.ztgame.com";
+
+    @Test
+    public void createIndexForsiteDir() {
+        try {
+            FSDirectory dir = FSDirectory.open(new File(indexDir));
+            StandardAnalyzer analyzer = new StandardAnalyzer(Version.LUCENE_4_9);
+            IndexWriterConfig conf = new IndexWriterConfig(Version.LUCENE_4_9, analyzer);
+            IndexWriter indexWriter = new IndexWriter(dir, conf);
+            File fileDir = new File(siteDir);
+            Collection<File> fileCollection = FileUtils.listFiles(fileDir, TrueFileFilter.INSTANCE, TrueFileFilter.INSTANCE);
+            for (File file : fileCollection) {
+                Document doc = new Document();
+                HtmlBean htmlBean = HtmlBeanUtils.parseHtml(file);
+                doc.add(new StringField("title", htmlBean.getTitle(), Field.Store.YES));
+                doc.add(new TextField("content", htmlBean.getContent(), Field.Store.YES));
+                doc.add(new StringField("url", htmlBean.getUrl(), Field.Store.YES));
+                indexWriter.addDocument(doc);
+            }
+            indexWriter.close();
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
 
     @Test
     public void createIndex() {
@@ -31,11 +58,12 @@ public class CreateIndex {
             IndexWriter indexWriter = new IndexWriter(fsDirectory, config);
             File file = new File(dataDir);
             File[] files = file.listFiles();
-            for (File f :files) {
+            for (File f : files) {
                 Document doc = new Document();
-                doc.add(new StringField("filename",f.getName(), Field.Store.YES));
-                doc.add(new TextField("content", FileUtils.readFileToString(f),Field.Store.YES));;
-                doc.add(new LongField("lastMOdify",f.lastModified(),Field.Store.YES));
+                doc.add(new StringField("filename", f.getName(), Field.Store.YES));
+                doc.add(new TextField("content", FileUtils.readFileToString(f), Field.Store.YES));
+                ;
+                doc.add(new LongField("lastMOdify", f.lastModified(), Field.Store.YES));
                 //2.使用indexWriter向document中写数据
                 indexWriter.addDocument(doc);
             }
---
+++ src/com/itcode/lucene/HtmlBean.java
@@ -0,0 +1,35 @@
+package com.itcode.lucene;
+
+/**
+ * Created by along on 17/9/29 10:45.
+ * Email:466210864@qq.com
+ */
+public class HtmlBean {
+    private String title;
+    private String content;
+    private String url;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getContent() {
+        return content;
+    }
+
+    public void setContent(String content) {
+        this.content = content;
+    }
+
+    public String getUrl() {
+        return url;
+    }
+
+    public void setUrl(String url) {
+        this.url = url;
+    }
+}
