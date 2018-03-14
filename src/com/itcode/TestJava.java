b37a412980c9e3868ba789a1cf2832508feb7f66
sunalong <sunalong@ztgame.com>
2018-03-14 11:17:42 +0800

    Java 执行shell命令

--- src/com/itcode/TestJava.java
+++ src/com/itcode/TestJava.java
@@ -1,5 +1,8 @@
 package com.itcode;
 
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+
 /**
  * Created by along on 17/9/18 11:21.
  * Email:466210864@qq.com
@@ -7,6 +10,25 @@ package com.itcode;
 public class TestJava {
 
     public static void main(String[] args) {
-        System.out.print(true + "<--->" + false);
+        System.out.println(true + "<--->" + false);
+        try {
+//            String shpath="/home/felven/word2vec/demo-classes.sh";
+//            Process ps = Runtime.getRuntime().exec(shpath);
+            String shcmd = "echo hello shell!";
+            Process ps = Runtime.getRuntime().exec(shcmd);
+            ps.waitFor();
+
+            BufferedReader br = new BufferedReader(new InputStreamReader(ps.getInputStream()));
+            StringBuffer sb = new StringBuffer();
+            String line;
+            while ((line = br.readLine()) != null) {
+                sb.append(line).append("\n");
+            }
+            String result = sb.toString();
+            System.out.println(result);
+        }
+        catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 }
